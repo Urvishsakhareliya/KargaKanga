4359bb296e89b0f3bc79f246a34196f678d06233
Urvish <urvishpatel3624@gmail.com>
2022-09-20 14:14:31 +0530

    update 20-09-2022 2:14

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-    $(".top-header .ri-menu-2-fill").click(function () {
-        $(".nav").addClass("nav-Active");
-        $(".BackLayer").addClass("BackLayer-Active");
-    });
-    $(".BackLayer").click(function () {
-        $(".nav").removeClass("nav-Active");
-        $(".BackLayer").removeClass("BackLayer-Active");
-    });
+  $(".top-header .ri-menu-2-fill").click(function () {
+    $(".nav").addClass("nav-Active");
+    $(".BackLayer").addClass("BackLayer-Active");
+    $("body").addClass("overflow-hidden");
+  });
+  $(".BackLayer").click(function () {
+    $(".nav").removeClass("nav-Active");
+    $(".BackLayer").removeClass("BackLayer-Active");
+    $("body").removeClass("overflow-hidden");
+  });
 });
